de7d572f27a712264d27aefcbd847ca7fd4fc9cc
Lorens Rafou <79044538+lorensrafou@users.noreply.github.com>
2026-01-14 04:35:12 -0800

    [any message]

--- app/main.py
+++ app/main.py
@@ -2,10 +2,13 @@ import sys
 
 
 def main():
-    # TODO: Uncomment the code below to pass the first stage
     sys.stdout.write("$ ")
     pass
 
+    # Wait for user input
+    command = input()
+    print(f"{command}: command not found")
 
+    
 if __name__ == "__main__":
     main()
